773766dc555675aa22bb5763c254783555585405
supriyapuri <supriyapuri11@gmail.com>
2022-12-09 16:15:51 -0800

    Package initialise

---
+++ init.R
@@ -0,0 +1,14 @@
+my_packages = c("Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+}
+
+
+
+devtools::install_github("stefanwilhelm/ShinyRatingInput")
+
+
+
