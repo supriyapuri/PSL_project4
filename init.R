a04c43fc264e86388fba7a1276c41495aed3a3e1
supriyapuri <supriyapuri11@gmail.com>
2022-12-09 16:40:29 -0800

    updated init.R to run packages

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
+my_packages = c("DT","Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -6,9 +6,6 @@ install_if_missing = function(p) {
   }
 }
 
-
-
-
-
+invisible(sapply(my_packages, install_if_missing))
 
 
