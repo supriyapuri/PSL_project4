26bdba6e3359177bac9e56eb300aba60d6f155d5
supriyapuri <supriyapuri11@gmail.com>
2022-12-10 03:37:44 -0800

    init changes

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("devtools", "DT","Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
+my_packages = c("pkgdown", "ragg","devtools", ,"DT","Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -10,3 +10,6 @@ invisible(sapply(my_packages, install_if_missing))
 
 
 devtools::install_github("stefanwilhelm/ShinyRatingInput")
+
+
+
