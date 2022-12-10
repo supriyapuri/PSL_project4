82871a70d254b8bb2394d34fa3e2fa6aeedace8f
supriyapuri <supriyapuri11@gmail.com>
2022-12-10 03:40:02 -0800

    init changes

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("pkgdown", "ragg","devtools", ,"DT","Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
+my_packages = c("pkgdown", "ragg","devtools", "DT","Matrix","shiny","tidytable","proxy","recommenderlab","reshape2","shinyjs","data.table","dplyr","shinyWidgets","tidyverse","reticulate","DT","data.table")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
